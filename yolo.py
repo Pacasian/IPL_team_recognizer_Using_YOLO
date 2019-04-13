8c4451abc3f7efbda06620ba6b4f0d556e77b60f
Sumith S Kumar <30325541+Pacasian@users.noreply.github.com>
2019-04-13 13:13:52 +0530

    Update yolo.py

--- yolo.py
+++ yolo.py
@@ -1,14 +1,11 @@
-# USAGE
-# python yolo.py --image images/baggage_claim.jpg --yolo yolo-coco
 
-# import the necessary packages
 import numpy as np
 import argparse
 import time
 import cv2
 import os
 
-# construct the argument parse and parse the arguments
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
 	help="path to input image")
@@ -20,17 +17,13 @@ ap.add_argument("-t", "--threshold", type=float, default=0.3,
 	help="threshold when applyong non-maxima suppression")
 args = vars(ap.parse_args())
 
-# load the COCO class labels our YOLO model was trained on
 labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
 LABELS = open(labelsPath).read().strip().split("\n")
 
 #checking for chennai super kings and mumbai indians
 
 def count_nonblack_np(img):
-    """Return the number of pixels in img that are not black.
-    img must be a Numpy array with colour values along the last axis.
 
-    """
     return img.any(axis=-1).sum()
 
 
@@ -88,9 +81,6 @@ image = cv2.imread(args["image"])
 ln = net.getLayerNames()
 ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
-# construct a blob from the input image and then perform a forward
-# pass of the YOLO object detector, giving us our bounding boxes and
-# associated probabilities
 blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),
 	swapRB=True, crop=False)
 net.setInput(blob)
@@ -101,8 +91,7 @@ end = time.time()
 # show timing information on YOLO
 print("[INFO] YOLO took {:.6f} seconds".format(end - start))
 
-# initialize our lists of detected bounding boxes, confidences, and
-# class IDs, respectively
+
 boxes = []
 confidences = []
 classIDs = []
@@ -111,46 +100,27 @@ classIDs = []
 for output in layerOutputs:
 	# loop over each of the detections
     for detection in output:
-		# extract the class ID and confidence (i.e., probability) of
-		# the current object detection
         scores = detection[5:]
         classID = np.argmax(scores)
         confidence = scores[classID]
 
-		# filter out weak predictions by ensuring the detected
-		# probability is greater than the minimum probability
+		
         if confidence > args["confidence"]:
-			# scale the bounding box coordinates back relative to the
-			# size of the image, keeping in mind that YOLO actually
-			# returns the center (x, y)-coordinates of the bounding
-			# box followed by the boxes' width and height
             box = detection[0:4] * np.array([W, H, W, H])
             (centerX, centerY, width, height) = box.astype("int")
-
-			# use the center (x, y)-coordinates to derive the top and
-			# and left corner of the bounding box
             x = int(centerX - (width / 2))
             y = int(centerY - (height / 2))
 
-			# update our list of bounding box coordinates, confidences,
-			# and class IDs
             boxes.append([x, y, int(width), int(height)])
             confidences.append(float(confidence))
             classIDs.append(classID)
 
-# apply non-maxima suppression to suppress weak, overlapping bounding
-# boxes
 idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],args["threshold"])
 
-# ensure at least one detection exists
 if len(idxs) > 0:
-	# loop over the indexes we are keeping
     for i in idxs.flatten():
-		# extract the bounding box coordinates
         (x, y) = (boxes[i][0], boxes[i][1])
         (w, h) = (boxes[i][2], boxes[i][3])
-
-		# draw a bounding box rectangle and label on the image
         color = [int(c) for c in COLORS[classIDs[i]]]
         cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
         text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
